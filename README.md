092228f43e335548427f049a30ea48004a27e9f1
Jun <zhangjun@kylinos.cn>
2021-05-18 21:33:43 +0800

    day518

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # my_algorithm
 my_own
+
+talk is more ,show me you code.
---
+++ array/MoveZeroes.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+/*
+给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+
+示例:
+
+输入: [0,1,0,3,12]
+输出: [1,3,12,0,0]
+说明:
+
+必须在原数组上操作，不能拷贝额外的数组。
+尽量减少操作次数。
+
+*/
+
+int arr[] = {0,1,5,0,12};
+
+void moveZeroes(int* nums, int numsSize){
+
+    int j = 0;
+/*
+    for(int i = 0;i<numsSize;++i){
+        if(nums[i]!=0){
+            nums[j] = nums[i];
+            if(i!=j){
+                nums[i] = 0;
+            }
+                j++;
+        }
+    }
+*/
+
+    /*
+    for(int i = 0;i <numsSize;i++){
+        if(nums[i]!=0)
+            nums[j++] = nums[i];
+    }
+
+    while(j<numsSize)
+        nums[j++] = 0;
+
+    */
+
+    int snow = 0;
+
+    for(int i = 0;i <numsSize;i++){
+        
+        if(nums[i] == 0){       
+            snow++;
+        }else if (snow > 0){
+
+            int t = nums[i];
+            nums[i] = 0;
+            nums[i - snow] = t;
+        }
+        
+    }
+
+}
+
+int main(){
+
+	moveZeroes(arr,5);	
+
+	for(int i=0;i<5;i++){
+		printf("arr[%d] is %d \t",i,arr[i]);
+	}
+	printf("\n");
+}
